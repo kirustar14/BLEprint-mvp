795dade08e6bc8b8e1d6b2cc5d3b6884da0bd239
kirustar14 <kiruthika.star14@gmail.com>
2026-02-12 10:23:01 -0800

    chenages

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
-// BLEprint Firebase Configuration
-
-window.FIREBASE_CONFIG = {
+// export the config as a module
+export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyA5nJvmn-6xcVhfv6XXq0UnLFMDXUVnlBM",
   authDomain: "bleprint-mvp-359e5.firebaseapp.com",
   projectId: "bleprint-mvp-359e5",
@@ -9,5 +8,5 @@ window.FIREBASE_CONFIG = {
   appId: "1:410659495978:web:dc68ad4b5202c117f9a353"
 };
 
-window.GOOGLE_CLIENT_ID =
+export const GOOGLE_CLIENT_ID =
   "410659495978-7vmkns7kv9i55f98718th13flcmtgqc0.apps.googleusercontent.com";
